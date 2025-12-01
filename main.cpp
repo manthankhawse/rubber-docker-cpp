e25760c4a7079157781e9a7e22c0febc5e3c7efa
manthankhawse <khawsemanthan246@gmail.com>
2025-12-01 19:45:42 +0530

    level 2 done ig

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sched.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
 #include <sys/mount.h>
 using namespace std;
 
 const char* new_root = "/new-root/rootfs";
 
 void contain(char *args[]){
+    if(unshare(CLONE_NEWNS)!=0){
+        perror("Error unsharing mount");
+        exit(1);
+    }
+
+    if(mount(NULL, "/", NULL, MS_PRIVATE | MS_REC, NULL)!=0){
+        perror("Error making / private");
+        exit(1);
+    }
     if(chdir(new_root)!=0){
         perror("Error changing root");
         exit(1);
     }
+
     if(chroot(new_root)!=0){
         perror("Error chrooting");
         exit(1);
@@ -19,11 +32,26 @@ void contain(char *args[]){
         perror("Error changing dir to /");
         exit(1);
     }
-    // add mount here "/proc"
+
     if(mount("proc", "/proc", "proc", 0, nullptr)!=0){
         perror("Error mounting proc");
         exit(1);
     }
+
+    if(mount("tmpfs", "/dev", "tmpfs", 0, nullptr)!=0){
+        perror("Error mounting dev");
+        exit(1);
+    }
+
+    if(mkdir("/dev/pts", 0755)!=0){
+        perror("Error creating directory");
+        exit(1);
+    }
+
+    mount("devpts", "/dev/pts", "devpts", 0, nullptr);
+    mknod("dev/null", 0666 | S_IFCHR, makedev(1,3));
+    mknod("dev/zero", 0666 | S_IFCHR, makedev(1,5));
+    mknod("dev/tty", 0666 | S_IFCHR, makedev(5,0));
     execvp(args[0], args);
     perror("execvp");
     exit(1);
